6a7a3843f8adc4d5854753cc2e5745603cc87409
sophakpanhasuon-commits <sophakpanhasuon@gmail.com>
2026-08-17 17:19:57 +0700

    Remove basename from BrowserRouter
    Removed basename from BrowserRouter in App component.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AppRoutes from './routes/AppRoutes';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/BuildReact">
+    <BrowserRouter>
       <AuthProvider>
         <ToastProvider>
           <AppRoutes />
